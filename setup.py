4736938ec25631f5bdfe134a9c22c8974a6d2316
Louis Cordier <lcordier@gmail.com>
2020-06-27 21:03:56 +0200

    Add setuptools build script.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='recipe_box',
+    version='0.1.0',
+    py_modules=['recipe_box'],
+    description='Utility to scrape recipes and put it in a local Zettelkasten.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    # https://pypi.org/classifiers/
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+    keywords='utility recipe scraper zettelkasten',
+    url='http://github.com/lcordier/recipe_box/',
+    author='Louis Cordier',
+    author_email='lcordier@gmail.com',
+    license='MIT',
+    install_requires=[
+        'recipe-scrapers',
+    ],
+    entry_points={
+        'console_scripts': [
+            'recipe_box=recipe_box:main',
+        ],
+    },
+)
