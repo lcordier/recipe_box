ce460f0dc3c63ba9d9e7650142b2478e52737cf7
Louis Cordier <lcordier@gmail.com>
2020-06-28 11:07:21 +0200

    Bump version for latest build.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme():
 
 setup(
     name='recipe_box',
-    version='0.1.1',
+    version='0.1.2',
     py_modules=['recipe_box'],
     description='Utility to scrape recipes and put it in a local Zettelkasten.',
     long_description=readme(),
